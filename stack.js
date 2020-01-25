c4db4846359da38ac05dd310d9f9727bb2a9a05b
kosmos434 <kosmos434@protonmail.com>
2020-01-25 13:34:24 -0600

    remove todos, change contexts

--- stack.js
+++ stack.js
@@ -1,20 +1,11 @@
 // TODO
-//// On right mouse click event
-//// Read highlighted text
-//// Save into a variable
-//// Create context menu item
-//// interpolate stackoverflow.com/search + highlightedWords
-//// save into searchURL
-//// context title changes to "Search stackoverflow for 'highlightedWords'"
-//// On click, opens new tab at searchURL
-// ! Figure out %s
 
 
 // * create the context menu
 browser.contextMenus.create({
     id: "stack",
-    title: "Search stackoverflow",
-    contexts: ["all"],
+    title: "Search stackoverflow for '%s'",
+    contexts: ["selection"],
 
     icons: {
         "16": "icons/stack16.png",
@@ -22,31 +13,18 @@ browser.contextMenus.create({
     }
 });
 
-// * on shown
-// browser.contextMenus.onShown.addListener(function(info, tab) {
-//     console.log(info);
-//     // ! if %s ??
-//     let updating = browser.contextMenus.update("stack", {
-//         title: "Search stackoverflow for '%s'"
-//     });
-// });
 
 // * onclick event
 browser.contextMenus.onClicked.addListener(function (info, tab) {
     let selectedText = info.selectionText.toString();
     // Turn the highlight into format: "this+is+our+search" 
-    let delimText = selectedText.split(" ").join("+");
+    let delimText = selectedText.trim().split(" ").join("+");
     let searchURL = `https://stackoverflow.com/search?q=${delimText}`;
     // new tab with our search term
     browser.tabs.create({
         url: searchURL
     });
 });
-
-
-
-
-
 
 
 // extension logs will appear in about:debugging -> 'inspect'
